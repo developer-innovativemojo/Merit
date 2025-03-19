33289b4d0f12c039215750c042dbb5d261de9198
developer <developer@innovativemojo.com>
2025-03-19 14:34:28 +0500

    animation in progress

--- src/components/NewLanding/HeroSection/index.tsx
+++ src/components/NewLanding/HeroSection/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import Text from "@/components/ui/Text";
 import Navbar from "../NavbarLanding";
+import ParticleBackground from "@/components/ui/ParticleBackground"; // Import particles
 
 import bg from "@/public/images/new-landing/Background Image.png";
 import cont from "@/public/images/new-landing/Container.png";
@@ -14,31 +15,44 @@ import right from "@/public/images/new-landing/Group 14.png";
 
 const HeroSection = () => {
   return (
-    <div className=" min-h-[863px] relative flex justify-center items-center px-5 mob:overflow-hidden">
+    <div className="min-h-[863px] relative flex justify-center items-center px-5 mob:overflow-hidden">
       <Navbar />
 
-      <div className=" flex justify-center items-center h-full">
-        <div className=" mx-auto space-y-[25px] relative z-20">
-          <Text
-            as="h1"
-            className="text-center font-inter text-[#30434D] mob:text-[40px] mob:leading-[49px]"
-          >
-            The
-            <span className="text-accentGreen"> Smartest, Fastest Way</span> to
-            Find the Right
-            <br />
-            Partners & Resources to Win More Work
-          </Text>
+      <div className="flex justify-center items-center h-full">
+        <div className="mx-auto relative z-20">
+          {/* Particle animation behind the heading */}
+          <div className="relative w-fit mx-auto">
+            <ParticleBackground />
+            <Text
+              as="h1"
+              className="relative text-center font-inter text-[#30434D] mob:text-[40px] mob:leading-[49px] z-10"
+            >
+              The
+              <span className="relative aurora-text">
+                {" "}
+                Smartest, Fastest Way
+              </span>{" "}
+              to Find the Right
+              <br />
+              Partners & Resources to Win More Work
+            </Text>
+          </div>
+
           <div className="mob:flex justify-between gap-[30px] w-full mt-[16px] mb-[88px] hidden">
             <Image className="w-[158px]" src={right} alt="bg" />
             <Image className="w-[155px]" src={left} alt="bg" />
           </div>
-          <Text className="text-accentGreen text-center font-bold text-[24px]  font-inter">
+          <Text className="text-accentGreen text-center mt-[23px] font-bold text-[24px] font-inter">
             Faster Teaming. Smarter Matches. Better Business.
           </Text>
         </div>
+
+        {/* ALL IMAGES ARE KEPT INTACT */}
         <Image
           className="absolute bottom-0 mx-auto"
+          data-aos="fade-up"
+          data-aos-duration="500"
+          data-aos-easing="ease-in-sine"
           src={bg}
           alt="bg"
           width={1092}
